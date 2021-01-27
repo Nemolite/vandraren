bf40981f2a20ad6392a615e830705d8d286e122b
Nemolite <g25092011@mail.ru>
2021-01-27 14:29:42 +0300

    Нaстройка блока мои работы

--- inc/my_functions.php
+++ inc/my_functions.php
@@ -46,4 +46,5 @@ function pagination($pages = '', $range = 4)
      }
 }
 
+
 ?>
--- index.php
+++ index.php
@@ -159,38 +159,9 @@ get_header();
   </a>
 </div>
 </div><!-- class="news-blog" -->
-<div class="main-job" id="main-job">
-  <div class="main-job-head">
-      <h1>Мои работы</h1>
-  </div>
-   <div class="main-job-txt">
-      <p>В этом разделе собраны некоторые сайты и проекты, которые разработаны мною за последний год.</p>
-  </div>
-  <div class="main-job-content">
-    <a href="http://sosh6-gshum.uxp.ru/" target="_blank">
-    <div class="job-block">
-      <img src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg2.jpeg" alt="">
-    </div>
-	</a>
-	<a href="http://gorka21.ru/" target="_blank">
-    <div class="job-block">
-      <img src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg5.jpeg" alt="">
-    </div>
-	<a>
-	<a href="http://det-sad19.ru/" target="_blank">
-    <div class="job-block">
-      <img src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg3.jpeg" alt="">
-    </div>
-    </a>
-  </div>
-  <div class="main-job-botton">
-    <a href="/portfolio/">
-      <div class="main-job-btn">
-          <h3>Посмотреть еще работы</h3>
-      </div>
-    </a>  
-  </div>
-</div><!-- main-job -->
+
+<!-- main-job block template-->
+<?php get_template_part( 'template-parts/main','job' );?>
 
 <div class="prices" id="prices_id">
     <div class="prices-head-txt">
