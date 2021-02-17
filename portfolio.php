c57df58e716645f9ef9cac1f5c27436e9ebfd957
Nemolite <g25092011@mail.ru>
2021-02-17 10:20:19 +0300

    Обновление портфолио

--- portfolio.php
+++ portfolio.php
@@ -19,78 +19,34 @@ get_header();
         	 <div class="pcontent-head">
         	  <h1>Портфолио по разработанным сайтам</h1>
            </div>
-           <a href="http://sport.uxp.ru/" target="_blank">
+
+
+
+
+
+           <?php for ($i=1;$i<=10;$i++) { ?>
+<?php 
+$url_job_site = 'url_job_site_'.$i;
+$img_upload = 'img_upload_'.$i;
+$info_text_jobs = 'info_text_jobs_'.$i;
+?>
+ <?php if (
+    (!empty(ot_get_option( $url_job_site  )))&&
+    (!empty(ot_get_option( $img_upload )))&&
+    (!empty(ot_get_option( $info_text_jobs ))) 
+  ) { ?>
+<a href="<?php echo ot_get_option( $url_job_site );?>" target="_blank">
         <div class="pcontent-blocks">
-        	<img id="blocks-images" src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg1.jpeg" alt="">
+        	<img id="blocks-images" src="<?php echo ot_get_option( $img_upload );?>" alt="">
           
             <div class="pcontent-blocks-txt" >
-            	<h3>Сайт Шумерлинских лыжников</h3>
+            	<h3><?php echo ot_get_option( $info_text_jobs );?></h3>
             </div>
 
         </div>
-            </a>
+<?php }?> 
+<?php }?> 
 
-
-
-          <a href="http://sosh6-gshum.uxp.ru/" target="_blank">
-        <div class="pcontent-blocks">
-        	<img id="blocks-images" src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg2.jpeg" alt="">
-            <div class="pcontent-blocks-txt dsp" >
-            	<h3>Сайт Школы </h3>
-            </div>
-
-        </div>
-            </a>
-           <a href="http://det-sad19.ru/" target="_blank">
-        <div class="pcontent-blocks">
-        	<img id="blocks-images" src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg3.jpeg" alt="">
-            <div class="pcontent-blocks-txt dsp" >
-            	<h3>Cайт Детского сада "Родничок" </h3>
-            </div>
-
-        </div>
-            </a>
-        <a href="https://dshi21.ru/" target="_blank">
-        <div class="pcontent-blocks">
-        	<img id="blocks-images" src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg4.jpeg" alt="">
-            <div class="pcontent-blocks-txt dsp" >
-            	<h3>Сайт Детской школа искусств </h3>
-            </div>
-
-        </div>
-            </a>
-
-        	
-
-        <a href="http://gorka21.ru/" target="_blank">
-        <div class="pcontent-blocks">
-        	<img id="blocks-images" src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg5.jpeg" alt="">
-            <div class="pcontent-blocks-txt dsp" >
-            	<h3>Интернет-магазин Горка </h3>
-            </div>
-
-        </div>
-            </a>
-
-         <a href="#">
-        <div class="pcontent-blocks">
-            <img id="blocks-images" src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg6.jpeg" alt="">
-            <div class="pcontent-blocks-txt dsp" >
-                <h3>Разработака сайта Детского сада </h3>
-            </div>
-
-        </div>
-            </a>
-
-         <a href="#">
-        <div class="pcontent-blocks">
-            <img id="blocks-images" src="<?php echo bloginfo('template_url'); ?>/vendor/img/pimg7.jpeg" alt="">
-            <div class="pcontent-blocks-txt dsp" >
-                <h3>Место для вашего проекта </h3>
-            </div>
-
-        </div>
-            </a>
 
         </div><!-- class="pcontent" -->
 
