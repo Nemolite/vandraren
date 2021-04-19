833f493f1bf64f8ca3f3a752b5980e44a6d8284d
Nemolite <g25092011@mail.ru>
2021-04-19 11:46:07 +0300

    Сделан вывод моих работ через произвольные поля

--- portfolio.php
+++ portfolio.php
@@ -6,52 +6,48 @@ Template Post Type: post, page, product
 
 get_header();
 ?>
-	 <div class="pmain">
-	 	<div class="pmain-menu">
-	 		<div class="pmain-menu-logo">
-	 			<a href="<?php echo get_home_url(); ?>">
-         <h1>VANDRAREN</h1>
-		</a> 
-	 		</div>
-	 	</div><!-- class="pmain-menu"-->
+<div class="pmain">
+        <div class="pmain-menu">
+		<div class="pmain-menu-logo">
+			<a href="<?php echo get_home_url(); ?>">
+                                <h1>VANDRAREN</h1>
+		        </a> 
+	 	</div>
+	</div><!-- class="pmain-menu"-->
 
         <div class="pcontent">
-        	 <div class="pcontent-head">
-        	  <h1>Портфолио по разработанным сайтам</h1>
-           </div>
+                <div class="pcontent-head">
+        	        <h1>Портфолио по разработанным сайтам</h1>
+                </div>
 
+                <?php
+                $args = array(
+                        'post_type' => 'myjobs',                               
+                        'post_status' => 'publish', 
+                        'posts_per_page' => -1,                           
+                );
 
-
-
-
-           <?php for ($i=1;$i<=11;$i++) { ?>
-<?php 
-$url_job_site = 'url_job_site_'.$i;
-$img_upload = 'img_upload_'.$i;
-$info_text_jobs = 'info_text_jobs_'.$i;
-?>
- <?php if (
-    (!empty(ot_get_option( $url_job_site  )))&&
-    (!empty(ot_get_option( $img_upload )))&&
-    (!empty(ot_get_option( $info_text_jobs ))) 
-  ) { ?>
-<a href="<?php echo ot_get_option( $url_job_site );?>" target="_blank">
-        <div class="pcontent-blocks">
-        	<img id="blocks-images" src="<?php echo ot_get_option( $img_upload );?>" alt="">
-          
-            <div class="pcontent-blocks-txt" >
-            	<h3><?php echo ot_get_option( $info_text_jobs );?></h3>
-            </div>
-
-        </div>
-<?php }?> 
-<?php }?> 
-
-
+                $query = new WP_Query($args);
+                if( $query->have_posts() ){
+                        while( $query->have_posts() ){            
+                        $query->the_post(); ?>         
+                                <a href="<?php echo vandraren_get_job_url(); ?>" target="_blank">
+                                        <div class="pcontent-blocks">
+                                                <img id="blocks-images" src="<?php echo get_the_post_thumbnail_url();?>" alt="<?php the_title(); ?>">
+        
+                                                <div class="pcontent-blocks-txt" >
+            	                                        <h3><?php the_title(); ?></h3>
+                                                 </div>
+                                        </div>
+                                </a>      
+        
+                <?php         
+                        }       
+                }
+                wp_reset_postdata();
+                ?>               
         </div><!-- class="pcontent" -->
-
-	 </div><!-- class="pmain" -->
-	
+</div><!-- class="pmain" -->
 
 <?php
 //get_sidebar();
